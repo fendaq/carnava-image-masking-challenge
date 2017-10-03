18d3dba7907fe803ceddca013252b841191d6ed5
git-hcLee <zerostalkerlee@outlook.com>
2017-10-04 03:23:04 +0800

    update

--- ensemble.py
+++ ensemble.py
@@ -168,6 +168,73 @@ def ensamble_png():
     log.write(' save_masks = %f min\n'%((timer() - total_start) / 60))
     log.write('\n')
 
+def ensamble_lmdb_png():
+
+    #out_dir = '/root/share/project/kaggle-carvana-cars/results/single/UNet512-peduo-label-00c'
+    #out_dir = '/root/share/project/kaggle-carvana-cars/results/single/UNet1024-peduo-label-01c'
+
+    out_dir_ = []
+    for i in range(0,5):
+    #for i in range(0,7):
+        out_dir_.append(params.out_dir + params.ensemble_dir + '_k%d'%(i+1))
+
+    #out_dir_.append(params.out_dir + params.ensemble_dir + '_single')
+    
+    final_out_dir = params.out_dir + params.ensemble_dir #+ '_post_train_no_src'
+
+    #logging, etc --------------------
+    os.makedirs(final_out_dir+'/submit/results',  exist_ok=True)
+    os.makedirs(final_out_dir + '/submit/test_mask', exist_ok=True)
+    backup_project_as_zip( os.path.dirname(os.path.realpath(__file__)), final_out_dir +'/backup/submit.code.zip')
+
+    log = Logger()
+    log.open(final_out_dir+'/log.submit.txt',mode='a')
+    log.write('\n--- [START %s] %s\n\n' % (datetime.now().strftime('%Y-%m-%d %H:%M:%S'), '-' * 64))
+    log.write('** some project setting **\n')
+    for i in range(len(out_dir_)):
+        log.write('*' + out_dir_[i] + '\n')
+
+
+    # read names
+    # split_file = CARVANA_DIR +'/split/'+ 'valid_v0_768'
+    # CARVANA_NUM_BLOCKS =1
+
+
+    split_file = CARVANA_DIR +'/split/'+ 'test_100064'
+    with open(split_file) as f:
+        names = f.readlines()
+    names = [name.strip()for name in names]
+    num_test = len(names)
+
+
+    rles=[]
+    #num_blocks = int(math.ceil(num_test/CSV_BLOCK_SIZE))
+    #print('num_blocks=%d'%num_blocks)
+    total_start = timer()
+    start = timer()
+    for i in range(len(names)): 
+        p = []
+        average = np.zeros((CARVANA_H,CARVANA_W),np.uint16)
+        for j in range(len(out_dir_)):
+            p.append(cv2.imread(out_dir_[j]+'/post_train/submit/test_mask/%s.png'%(names[i]),cv2.IMREAD_GRAYSCALE))
+            p[j] = p[j].astype(np.uint8)
+            
+            average += p[j]
+        
+        #average = average/5
+        average = average/len(out_dir_)
+        
+        cv2.imwrite(final_out_dir+'/submit/test_mask/%s.png'%(names[i]), average.astype(np.uint8))
+
+        print('\r current_num: %d'%(i), end='', flush=True)
+        if i%5000 == 0:
+            log.write(' [num: %d] \n'%(i))
+            log.write('\t time = %0.2f min ,check dir_num = %d\n'%((timer() - start)/60, len(out_dir_)))
+            start = timer()
+    
+    log.write(' save_masks = %f min\n'%((timer() - total_start) / 60))
+    log.write('\n')
+
 def run_submit_ensemble():
 
     #out_dir = '/root/share/project/kaggle-carvana-cars/results/single/UNet512-peduo-label-00c'
